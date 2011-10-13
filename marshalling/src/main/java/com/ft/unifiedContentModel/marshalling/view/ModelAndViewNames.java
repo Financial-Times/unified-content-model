ce67b03297c8af25f519be1ee53f6b12762f709d
Dan Massie <dan.massie@ft.com>
2011-10-13 16:47:23 +0000

    IP857 - moving some content api marshalling to universal content model

---
+++ marshalling/src/main/java/com/ft/unifiedContentModel/marshalling/view/ModelAndViewNames.java
@@ -0,0 +1,10 @@
+package com.ft.unifiedContentModel.marshalling.view;
+
+public abstract class ModelAndViewNames {
+	
+	private ModelAndViewNames() {}
+	
+	public static final String MODEL_NAME = "model";
+	public static final String VIEW_NAME = "view";
+
+}
---
+++ model/src/main/java/com/ft/unifiedContentModel/model/response/package-info.java
@@ -0,0 +1,16 @@
+@XmlSchema(namespace = com.ft.unifiedContentModel.model.XSDs.CONTENT_API_NAMESPACE,
+    xmlns = { 
+        @XmlNs(namespaceURI = com.ft.unifiedContentModel.model.XSDs.ITEM_RESPONSE_NAMESPACE, prefix = com.ft.unifiedContentModel.model.XSDs.ITEM_RESPONSE_NAMESPACE_PREFIX),
+        @XmlNs(namespaceURI = com.ft.unifiedContentModel.model.XSDs.ITEMS_RESPONSE_NAMESPACE, prefix = com.ft.unifiedContentModel.model.XSDs.ITEMS_RESPONSE_NAMESPACE_PREFIX),
+        @XmlNs(namespaceURI = com.ft.unifiedContentModel.model.XSDs.CONTENTITEM_NAMESPACE, prefix=com.ft.unifiedContentModel.model.XSDs.CONTENTITEM_NAMESPACE_PREFIX),
+        @XmlNs(namespaceURI = com.ft.unifiedContentModel.model.XSDs.METADATA_NAMESPACE, prefix = com.ft.unifiedContentModel.model.XSDs.METADATA_NAMESPACE_PREFIX),
+        @XmlNs(namespaceURI = com.ft.unifiedContentModel.model.XSDs.IMAGE_NAMESPACE, prefix = com.ft.unifiedContentModel.model.XSDs.IMAGE_NAMESPACE_PREFIX),
+        @XmlNs(namespaceURI = com.ft.unifiedContentModel.model.XSDs.ASPECT_NAMESPACE, prefix = com.ft.unifiedContentModel.model.XSDs.ASPECT_NAMESPACE_PREFIX),
+        @XmlNs(namespaceURI = com.ft.unifiedContentModel.model.XSDs.FIELD_NAMESPACE, prefix = com.ft.unifiedContentModel.model.XSDs.FIELD_NAMESPACE_PREFIX),
+        @XmlNs(namespaceURI = com.ft.unifiedContentModel.model.XSDs.SCHEMAINSTANCE_NAMESPACE, prefix = com.ft.unifiedContentModel.model.XSDs.SCHEMAINSTANCE_NAMESPACE_PREFIX)
+    },
+    elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.ft.unifiedContentModel.model.response;
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlSchema;
+
