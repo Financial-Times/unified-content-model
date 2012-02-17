9f04d9937f6d9590912fbe2d0ff62252736c0cc6
A Abiri <a.biri@ft.com>
2012-02-17 16:43:56 +0000

    US19553: Added handling of IO Exceptions and 503 response from the DP.

---
+++ ws/src/main/java/com/ft/unifiedContentModel/ws/exception/ServiceUnavailableException.java
@@ -0,0 +1,12 @@
+package com.ft.unifiedContentModel.ws.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+public class ServiceUnavailableException extends RuntimeException {
+
+	public ServiceUnavailableException(Throwable cause) {
+		super(cause);
+	}
+}
