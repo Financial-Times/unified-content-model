9aba9a5113a48372789b95f327e3f883c7509505
Alberto Facia <alberto.facia@ft.com>
2012-07-18 11:41:14 +0000

    IP931 US23311 Fixed problem with content-item json property on NotificationData and NotificationDataView

--- model/src/main/java/com/ft/unifiedContentModel/model/notification/NotificationDataImpl.java
+++ model/src/main/java/com/ft/unifiedContentModel/model/notification/NotificationDataImpl.java
@@ -1,6 +1,7 @@
 package com.ft.unifiedContentModel.model.notification;
 
 import java.io.Serializable;
+
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.annotate.JsonPropertyOrder;
 import org.codehaus.jackson.map.annotate.JsonDeserialize;
@@ -20,12 +21,12 @@ public class NotificationDataImpl implements NotificationData, Serializable {
     }
 
     @Override
+    @JsonProperty("content-item")
     public NotificationReference getNotificationReference() {
         return notificationReference;
     }
 
     @JsonDeserialize(as = ContentItemReference.class)
-    @JsonProperty("content-item")
     public void setNotificationReference(NotificationReference notificationReference) {
         this.notificationReference = notificationReference;
     }
