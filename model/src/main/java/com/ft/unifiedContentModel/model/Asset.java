34af02393456a78fbaf12b81980507513cb98b0b
David Sammut <david.sammut@ft.com>
2012-10-01 13:27:29 +0000

    US24373 changed the pull quote asset type use camel case

--- model/src/main/java/com/ft/unifiedContentModel/model/Asset.java
+++ model/src/main/java/com/ft/unifiedContentModel/model/Asset.java
@@ -7,7 +7,7 @@ import org.codehaus.jackson.annotate.JsonTypeInfo;
     @JsonSubTypes({
            @JsonSubTypes.Type(value=Slideshow.class, name="slideshow"),
            @JsonSubTypes.Type(value=VideoAsset.class, name="video"),
-           @JsonSubTypes.Type(value=PullQuote.class, name="pullquote"),
+           @JsonSubTypes.Type(value=PullQuote.class, name="pullQuote"),
            @JsonSubTypes.Type(value=InteractiveGraphic.class, name="interactiveGraphic")
     })
 public interface Asset {
