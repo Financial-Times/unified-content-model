b95dbab53d6b48f0a516130bcca4f0b2167f9e52
Neil Green <neil@melessoftware.com>
2013-08-16 13:16:57 +0100

    US32326 add request id header to NDC

--- ws/src/main/java/com/ft/api/ucm/rest/filter/UserContextLoggingFilter.java
+++ ws/src/main/java/com/ft/api/ucm/rest/filter/UserContextLoggingFilter.java
@@ -8,12 +8,14 @@ import org.springframework.web.filter.Log4jNestedDiagnosticContextFilter;
 public class UserContextLoggingFilter extends Log4jNestedDiagnosticContextFilter {
 
 	public static final String API_KEY_HEADER = "X-Api-Key";
-	public static final String USER_IP_HEADER = "X-Forwarded-For";
     public static final String CONTENT_CONTROL_POLICY_HEADER = "X-FT-API-Content-Control-Policy";
-	
-	protected static final String API_KEY_LABEL = "apiKey";
-	protected static final String USER_IP_LABEL = "IP";
+    public static final String USER_IP_HEADER = "X-Forwarded-For";
+    public static final String REQUEST_ID_HEADER = "X-Request-Id";
+
+    protected static final String API_KEY_LABEL = "apiKey";
+    protected static final String USER_IP_LABEL = "IP";
     protected static final String CONTENT_CONTROL_POLCIY_LABEL = "contentControlPolicy";
+    protected static final String REQUEST_ID_LABEL = "requestId";
 
     @Override
 	protected String createMessage(HttpServletRequest request, String prefix,
@@ -25,25 +27,31 @@ public class UserContextLoggingFilter extends Log4jNestedDiagnosticContextFilter
 		return msg.toString();
 	}
 
-	private void appendCurrentLoggingContext(HttpServletRequest request, StringBuilder message) {
+	private void appendCurrentLoggingContext(HttpServletRequest request, StringBuilder appendTo) {
         String apikey = request.getHeader(API_KEY_HEADER);
 		String ip = request.getHeader(USER_IP_HEADER);
         String contentControlPolicy = request.getHeader(CONTENT_CONTROL_POLICY_HEADER);
+        String requestId = request.getHeader(REQUEST_ID_HEADER);
 		
         if(!StringUtils.isBlank(apikey)) {
-			message.append(API_KEY_LABEL)
-                   .append('=')
-                   .append(apikey)
-                   .append(' ');
-		}
+			appendTo.append(API_KEY_LABEL)
+                    .append('=')
+                    .append(apikey)
+                    .append(' ');
+        }
 		
-		message.append(USER_IP_LABEL)
-               .append('=')
-               .append(StringUtils.isBlank(ip) ? request.getRemoteAddr() : ip)
-               .append(' ');
+		appendTo.append(USER_IP_LABEL)
+                .append('=')
+                .append(StringUtils.isBlank(ip) ? request.getRemoteAddr() : ip)
+                .append(' ');
 
-        message.append(this.CONTENT_CONTROL_POLCIY_LABEL)
-               .append('=')
-               .append(contentControlPolicy==null ? "" : contentControlPolicy);
-	}
+        appendTo.append(CONTENT_CONTROL_POLCIY_LABEL)
+                .append('=')
+                .append(contentControlPolicy == null ? "" : contentControlPolicy)
+                .append(' ');
+
+        appendTo.append(REQUEST_ID_LABEL)
+                .append('=')
+                .append(requestId==null ? "" : requestId);
+    }
 }
--- ws/src/test/java/com/ft/api/ucm/rest/filter/UserContextLoggingFilterTest.java
+++ ws/src/test/java/com/ft/api/ucm/rest/filter/UserContextLoggingFilterTest.java
@@ -15,11 +15,13 @@ public class UserContextLoggingFilterTest {
 
     @Mock
     public HttpServletRequest request;
+    public static final String prefix = "Hello";
 
-    public static String prefix = "Hello";
-    public static String suffix = "Goodbye";
-    public static String apiKeyHeader = "apiKeyHeader";
-    public static String userIpHeader = "userIpHeader";
+    public static final String suffix = "Goodbye";
+    public static final String apiKeyHeader = "tApiKeyHeader";
+    public static final String userIpHeader = "theUserIpHeader";
+    public static final String contentControlHeader = "theContentControlHeader";
+    public static final String requestId = "theRequestId";
 
     public UserContextLoggingFilter userContextLoggingFilter;
 
@@ -31,15 +33,17 @@ public class UserContextLoggingFilterTest {
     @Test
     public void testCreateMessage(){
 
-        when(request.getHeader(UserContextLoggingFilter.API_KEY_HEADER)).thenReturn("apiKeyHeader");
-        when(request.getHeader(UserContextLoggingFilter.USER_IP_HEADER)).thenReturn("userIpHeader");
-        when(request.getHeader(UserContextLoggingFilter.CONTENT_CONTROL_POLICY_HEADER)).thenReturn("contentControlHeader");
+        when(request.getHeader(UserContextLoggingFilter.API_KEY_HEADER)).thenReturn(apiKeyHeader);
+        when(request.getHeader(UserContextLoggingFilter.USER_IP_HEADER)).thenReturn(userIpHeader);
+        when(request.getHeader(UserContextLoggingFilter.CONTENT_CONTROL_POLICY_HEADER)).thenReturn(contentControlHeader);
+        when(request.getHeader(UserContextLoggingFilter.REQUEST_ID_HEADER)).thenReturn(requestId);
         String s = userContextLoggingFilter.createMessage(request, prefix, suffix);
 
         assertEquals(s, prefix
                 + UserContextLoggingFilter.API_KEY_LABEL + "=" + apiKeyHeader + " "
                 + UserContextLoggingFilter.USER_IP_LABEL  + "=" + userIpHeader + " "
-                + UserContextLoggingFilter.CONTENT_CONTROL_POLCIY_LABEL + "=" + "contentControlHeader"
+                + UserContextLoggingFilter.CONTENT_CONTROL_POLCIY_LABEL + "=" + contentControlHeader + " "
+                + UserContextLoggingFilter.REQUEST_ID_LABEL + "=" + requestId
                 + suffix);
 
     }
