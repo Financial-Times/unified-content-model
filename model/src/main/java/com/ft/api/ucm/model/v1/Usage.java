f3b1d40dfd0e9736b7057e57f0eaf1af10db92a0
Serban Pop <serban.pop@ft.com>
2014-04-01 16:27:25 +0300

    US41093 FT Weekend Tagging - replaced SortedSet with Set in UsageImpl

--- model/src/main/java/com/ft/api/ucm/model/v1/Usage.java
+++ model/src/main/java/com/ft/api/ucm/model/v1/Usage.java
@@ -4,11 +4,11 @@ import com.ft.api.ucm.model.v1.metadata.Term;
 import org.codehaus.jackson.map.annotate.JsonDeserialize;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 
-import java.util.SortedSet;
+import java.util.Set;
 
 @JsonSerialize(include = JsonSerialize.Inclusion.NON_EMPTY)
 @JsonDeserialize(as = UsageImpl.class)
 public interface Usage {
 
-    SortedSet<Term> getPublication();
+    Set<Term> getPublication();
 }
--- model/src/main/java/com/ft/api/ucm/model/v1/UsageImpl.java
+++ model/src/main/java/com/ft/api/ucm/model/v1/UsageImpl.java
@@ -2,20 +2,21 @@ package com.ft.api.ucm.model.v1;
 
 import com.ft.api.ucm.model.v1.metadata.Term;
 
+import java.util.Set;
 import java.util.SortedSet;
 
 public class UsageImpl implements Usage {
 
-    private SortedSet<Term> publication;
+    private Set<Term> publication;
 
     public UsageImpl() {};
 
-    public UsageImpl(SortedSet<Term> publication) {
+    public UsageImpl(Set<Term> publication) {
         this.publication = publication;
     }
 
     @Override
-    public SortedSet<Term> getPublication() {
+    public Set<Term> getPublication() {
         return publication;
     }
 
